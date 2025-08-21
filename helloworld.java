ed618a934559e0ab1b4462bc37d6b49d6f61b8f2
sreemukhi748 <sreemukhigupta@gmail.com>
2025-08-21 12:06:53 +0530

    added helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class helloworld {
+public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("helloworld");
     }
 }
